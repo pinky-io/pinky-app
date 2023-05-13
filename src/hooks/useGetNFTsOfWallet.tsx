8bd8b17205b0ff15b69df07c2df4a018c49bc805
Sinane <sin.sim@outlook.fr>
2023-05-13 02:46:59 +0100

    fix: useGetNftOfAWallet loader

--- src/hooks/useGetNFTsOfWallet.tsx
+++ src/hooks/useGetNFTsOfWallet.tsx
@@ -1,4 +1,5 @@
-import { Network, Alchemy } from "alchemy-sdk";
+import { Network, Alchemy, OwnedNft } from "alchemy-sdk";
+import { useEffect, useState } from "react";
 
 const settings = {
   apiKey: "x97VSdTdeVeFEUpCDRH1ENsSyi5PYPIP",
@@ -8,7 +9,29 @@ const settings = {
 const alchemy = new Alchemy(settings);
 
 export const useGetNFtsOfWallet = async (walletAddress: string) => {
-  const nfts = await alchemy.nft.getNftsForOwner(walletAddress);
+  const [nfts, setNfts] = useState<OwnedNft[]>([]);
+  const [loading, setLoading] = useState(false);
 
-  return nfts;
+  useEffect(() => {
+    async function fetchNfts() {
+      setLoading(true);
+
+      try {
+        const nftsFetched = await alchemy.nft.getNftsForOwner(walletAddress);
+        setNfts(nftsFetched.ownedNfts);
+      } catch (error) {
+        console.error(error);
+
+        const nftsFetched = await alchemy.nft.getNftsForOwner(walletAddress);
+        setNfts(nftsFetched.ownedNfts);
+        setLoading(false);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchNfts();
+  }, [walletAddress]);
+
+  return { loading, nfts };
 };
