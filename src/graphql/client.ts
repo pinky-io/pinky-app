bc1866cfae7259268112de16e59e59920e24df06
mnaji42 <mehdi.n.pro@gmail.com>
2023-05-14 04:17:52 +0100

    feat(thegraph) update subgraph version

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client"
 
 const THE_GRAPH_ENDPOINT =
-  "https://api.studio.thegraph.com/query/46763/test0/v0.0.3"
+  "https://api.studio.thegraph.com/query/46763/test0/v0.0.4"
 
 export const client = new ApolloClient({
   uri: THE_GRAPH_ENDPOINT,
